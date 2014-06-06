0c1930773d7053053a26fbb36e56e13b485ed20c
IniZio <chowkachun1@gmail.com>
2014-06-06 09:30:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 
+class student{
+  private:
+    int id_num;     // 1000
+    int form;
+    int rank;
+    string name;
+    int score;
+  public:
+    
+};
 
+void get_answersheets(string path){
+  FILE * fp;
+  fp = fopen ("answer_sheets.txt", "r");
+  if (fp.is_open)
+  
+}
 
 int main(){
   return 0;
